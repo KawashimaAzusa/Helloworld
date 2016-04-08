e54cb0808b0239b37fc710af4cf59584c28fbc9b
g14920ka <g14920ka@s30440.ACTSUDA>
2016-04-08 11:24:59 +0900

    文字列を表示するようにしました

--- src/helloworld2/HelloWorld2.java
+++ src/helloworld2/HelloWorld2.java
@@ -16,7 +16,7 @@ public class HelloWorld2 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("システム開発　第一回");
+        System.out.println("システム開発第一回");
     }
     
 }
